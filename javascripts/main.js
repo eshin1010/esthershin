b703803b5d76c86b492d4bc3c40f99d9c64a5dff
eshin1010 <eshin1010@gmail.com>
2017-12-27 22:38:37 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,3 +23,21 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+//SLIDES
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
